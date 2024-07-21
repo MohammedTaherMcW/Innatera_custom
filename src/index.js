38c2d9fc213eaa22d973dc20fc735f6001d31bf7
MohammedTaherMcW <mohammed.rasheed@multicorewareinc.com>
2024-07-21 19:36:55 +0530

    Imported the Custom react Component to the index.js

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { AppContainer } from 'react-hot-loader';
+import { createStore } from 'redux';
+import * as store from '../store/index.js'
+import Custom from '../modules/custom/containers/custom.jsx';
+
+
+window.addEventListener('error', (err) => {
+  if (reportException) {
+    reportException(err, true);
+  } else {
+    console.error("ReportException not defined");
+  }
+});
+
+// Main render function
+const renderApp = () => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store.getStore()}>
+        <Custom />
+      </Provider>
+    </AppContainer>,
+    document.getElementById('root')
+  );
+};
+
+// Initial render
+renderApp();
+
+// Hot Module Replacement API
+if (module && module.hot) {
+    module.hot.accept('../modules/custom/containers/custom.jsx', () => {
+    renderApp();
+  });
+}
