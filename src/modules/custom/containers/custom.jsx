ac8e65f8662c65d5245d58662365b411f2181868
MohammedTaherMcW <mohammed.rasheed@multicorewareinc.com>
2024-07-24 15:22:24 +0530

    Added the info tag  in Custom Page

--- src/modules/custom/containers/custom.jsx
+++ src/modules/custom/containers/custom.jsx
@@ -1,9 +1,10 @@
 import * as actions from '../actions';
 import * as path from '../../core/path';
-import { Form, Icon, Input } from 'antd';
+import { Form, Icon, Input,Tooltip } from 'antd';
 // filename app.js
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { InfoCircleTwoTone } from '@ant-design/icons';
 
 import React from 'react';
 import { connect } from 'react-redux';
@@ -19,26 +20,31 @@ class Custom extends React.Component {
     }
     render() {        
         return (
-                <Form onSubmit={(e) => {e.preventDefault(); this.sendtargets(this.state.targets); window.location.reload();}}>
+            <Form onSubmit={(e) => {e.preventDefault(); this.sendtargets(this.state.targets); window.location.reload();}}>
                     <h1 className="display-4">Custom Targets</h1>
                     <div className="alert alert-warning" role="alert">
-                        <h4 className="alert-heading">Alert!</h4>
-                        <p className="mb-0" style={{fontSize: '18px'}}>The order of Execution of the targets will be same as the order of the targets specified.</p>
+                        <p className="mb-0" style={{fontSize: '18px'}}>Warning! : The order of Execution of the targets will be same as the order of the targets specified.</p>
                     </div>
-                    <Form.Item
-                        label="Targets"
-                        labelCol={{ span: 4 }}
-                        wrapperCol={{ span: 8, style: { height: '40px' } }}
-                    >
+                <Form.Item 
+                    label= "Targets"
+                    labelCol={{ span: 4, offset: 0 }} 
+                    wrapperCol={{ span: 4, offset: 0 }} 
+                    style={{ display: 'flex' }}
+                >
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
                         <Input
                             size="small"
-                            style={{ height: '30px' }}
+                            style={{ height: '30px', fontSize: '16px', flexGrow: 1 }}
                             value={this.state.targets}
                             onChange={(event) => this.setState({ targets: event.target.value })}
                         />
-                    </Form.Item>    
+                        <Tooltip title="Note: Multiple targets should be separated by Comma.">
+                            <InfoCircleTwoTone style={{ marginLeft: '8px', fontSize: '16px' }} />
+                        </Tooltip>
+                    </div>
+                    </Form.Item>
                     <Form.Item
-                        wrapperCol={{ span: 4, offset: 4, style: { height: '60px' } }}
+                        wrapperCol={{ span: 3, offset: 4, style: { height: '60px' } }}
                     >
                         <Input
                             size="small"
